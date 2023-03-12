74339673e76dd292f381c7af465b9bf69b1d5a5b
Murtaza Mister <murtazamister1@gmail.com>
2023-03-12 14:31:36 +0530

    Added color picker

--- src/Pages/TagPage/TagPage.jsx
+++ src/Pages/TagPage/TagPage.jsx
@@ -6,50 +6,27 @@ import StatusContext from '../../Contexts/StatusContext';
 import TimeContext from '../../Contexts/TimeContext';
 import './TagPage.css'
 import { Link, useNavigate } from 'react-router-dom';
-import {SketchPicker} from 'react-color';
-
-import 'react-color-picker/index.css'
+import ColorPicker from '../../Components/ColorPicker/ColorPicker';
 
 function TagPage(){
 
     useEffect(()=>{
         setTimeout(()=>{
             document.querySelector('.tag-body').style.height = "50%"
-            setTimeout(()=>{
-                // document.querySelector('.form-holder').style.width = "65%"
-                setPlay_Pause('pause')
-            },1000)
         },1000)
     },[])
 
-    onClick = () => {
-          setShowPicker(showPicker)
-    };
- 
-    onClose = () => {
-      this.setState({ 
-        showPicker: false 
-      })
-    };
- 
-    onChange = (color) => {
-        this.setState({ 
-          color: color.rgb 
-        })
-    };
-
     const [name, setName] = useState("");
     const [color, setColor] = useState({
-        r: '225',
-        g: '155',
-        b: '99',
-        a: '2'
-    });
+      r: '225',
+      g: '155',
+      b: '99',
+      a: '2',
+    })
     
     const [invalid, setInvalid] = useState(false);
     const {user, setUser} = useContext(UserContext);
     const navigate = useNavigate()
-    const [showPicker, setShowPicker] = useState(false)
 
     const handleTag = async (e)=>{
         e.preventDefault();
@@ -71,54 +48,6 @@ function TagPage(){
         }
     }
 
-    onClick = () => {
-        this.setState({ 
-          showPicker: !this.state.showPicker 
-        })
-    };
- 
-    onClose = () => {
-      this.setState({ 
-        showPicker: false 
-      })
-    };
- 
-    onChange = (color) => {
-        this.setState({ 
-          color: color.rgb 
-        })
-    };
-
-    const styles = reactCSS({
-        'default': {
-          color: {
-            width: '40px',
-            height: '15px',
-            borderRadius: '3px',
-            background: `rgba(${ this.state.color.r }, ${ this.state.color.g }, ${ this.state.color.b }, ${ this.state.color.a })`,
-          },
-          popover: {
-            position: 'absolute',
-            zIndex: '3',
-          },
-          cover: {
-            position: 'fixed',
-            top: '0px',
-            right: '0px',
-            bottom: '0px',
-            left: '0px',
-          },
-          swatch: {
-            padding: '6px',
-            background: '#ffffff',
-            borderRadius: '2px',
-            cursor: 'pointer',
-            display: 'inline-block',
-            boxShadow: '0 0 0 1px rgba(0,0,0,.2)',
-          },          
-        },
-      });
-
     return (
         <div className="tag-body">
             <div className='form-holder'>
@@ -131,17 +60,9 @@ function TagPage(){
                             <label htmlFor="name">Name</label>
                             <input value={name} onChange={(e) => setName(e.target.value)} type="text" name="name" id="name" />
                         </div>
-                        <div className='form-element'>
-                        <div>
-                            <div style={ styles.swatch } onClick={ onClick }>
-                                <div style={ styles.color } />
-                            </div>
-                            { this.state.showPicker ? <div style={ styles.popover }>
-                                <div style={ styles.cover } onClick={ this.onClose }/>
-                                <SketchPicker color={ this.state.color } onChange={ this.onChange } />
-                            </div> : null }
-                    
-                            </div>
+                        <div className='form-element' style={{display:"flex"}}>
+                          <label htmlFor="colorPicker">Color</label>
+                          <ColorPicker color={color} id="colorPicker"/>
                         </div>
                         <div className="form-element">
                             <input type="submit" value="Submit" />
